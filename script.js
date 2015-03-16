f9b4e7c4f0695d2784632fb9ba72a4a32a65341c
Patrick Liu <patrickhliu@gmail.com>
2015-03-15 21:02:29 -0700

    changed js, single event listener on game board

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /*  This file is written in vanilla javascript */
 
-var turns = 0;                                                      // track the turn count, even turn is "O", odd turn is "X"              
+var turns = 0;                                                      // track turn count, even turn is "O", odd turn is "X"              
 var sqSet = document.getElementsByClassName("square");              // reference to node list of the 9 square <li> elements
-var reset = document.querySelector(".reset");                       // reference to reset button at the bottom of page
+var reset = document.querySelector(".reset");                       // reference to reset button
+var gameBoard = document.querySelector('.board');                   // reference to the <section> containing the 9 squares
 
 var sq1 = document.querySelector(".sq1");                           // reference to each individual square
 var sq2 = document.querySelector(".sq2");
@@ -20,7 +21,7 @@ function hasClass(elemArr, clName) {                                // Call this
     });
 }
 
-function resetSquare(elem) {                                        // Call this function to reset all square <li> to their initial HTML status
+function resetSquare(elem) {                                        // Call this function to reset all square <li> to their initial status
     elem.textContent = '';
     elem.classList.remove('disable');
     elem.classList.remove('o');
@@ -28,7 +29,7 @@ function resetSquare(elem) {                                        // Call this
 }
 
 function resetSquareAll() {                                         // Call this function to reset all 9 <li> elements at once
-    for (var j=0; j<sqSet.length; j++) {
+    for (var j = 0; j < sqSet.length; j++) {
         resetSquare( sqSet[j] );
     }
 
@@ -36,68 +37,57 @@ function resetSquareAll() {                                         // Call this
 }
 
 // Call this function to check if 3 straight O's occur on the board
-function check_O () {
+function didOWin () {
     if( hasClass( [sq1, sq2, sq3], "o") || hasClass( [sq4, sq5, sq6], "o") || hasClass( [sq7, sq8, sq9], "o")  ||
         hasClass( [sq1, sq4, sq7], "o") || hasClass( [sq2, sq5, sq8], "o") || hasClass( [sq3, sq6, sq9], "o")  ||
         hasClass( [sq1, sq5, sq9], "o") || hasClass( [sq3, sq5, sq7], "o") ) {
-        return true;
-    }
-    else {
-        return false;
+         alert("O Winsies!!  Press OK to start new game");       // if yes, alert game winner
+         turns = 0;                                              // reset turn count
+         resetSquareAll();                                       // reset HTML of all <li> squares
     }
 }
 
 // Call this function to check if 3 straight X's occur on the board
-function check_X () {
+function didXWin () {
     if(hasClass( [sq1, sq2, sq3], "x") || hasClass( [sq4, sq5, sq6], "x") || hasClass( [sq7, sq8, sq9], "x")  ||
        hasClass( [sq1, sq4, sq7], "x") || hasClass( [sq2, sq5, sq8], "x") || hasClass( [sq3, sq6, sq9], "x")  ||
        hasClass( [sq1, sq5, sq9], "x") || hasClass( [sq3, sq5, sq7], "x") ) {
-            return true;
-    }
-    else {
-        return false;
+            alert("X Winsies!!  Press OK to start new game");       // if yes, alert game winner
+            turns = 0;                                              // reset turn count
+            resetSquareAll();                                       // reset HTML of all <li> squares
     }
 }
 
-// A long click event handler for each of the 9 squares
-for ( var i = 0; i < sqSet.length; i++ ) {
-    sqSet[i].addEventListener("click", function() {     
-        if( hasClass( [this], "disable") ) {                            // Check if user clicks on an already marked square, if yes, alert user
+gameBoard.addEventListener('click', checkGameProgress, false);      // event handler for click on the game board container
+
+function checkGameProgress(e) {
+    if (e.target.classList.contains('square')) {                    // if the user clicked on any of the 9 squares
+        if (  hasClass( [e.target], "disable") ) {                  // if user clicks on an already marked square, alert user
             alert("That square is already marked..");
         }
-        else if ( turns % 2 === 0 ) {                                   // Check if turn count is even, if so mark square as "O"
+        else if ( turns % 2 === 0 ) {                               // if turn count is even...
             turns++;
-            this.textContent = "o";                                     // set element's inner text to "O"
-            this.classList.add('disable');                              // add class "disable" as a flag indicating square can't be marked again
-            this.classList.add('o');                                    // add class "o", just changes background color
-            if( check_O() ) {                                           // check if 3 straight O's have occured
-                console.log("O winsies!");                          
-                alert("O Winsies!!  Press OK to start new game");       // if yes, alert game winner
-                turns = 0;                                              // reset turn count
-                resetSquareAll();                                       // reset HTML of all <li> squares
-            }
+            e.target.textContent = "o";                             // set element's inner text to "O"
+            e.target.classList.add('disable');                      // add class "disable" as a flag indicating square can't be marked again
+            e.target.classList.add('o');                            // add class "o", just changes background color
+            didOWin();
         }
-        else if ( turns % 2 === 1 ) {
-            turns++;                                                    // else means turn count is odd, mark square as "X"
-            this.textContent = "x";                                     // set inner text of element to "X"
-            this.classList.add('disable');                              // add class "disable" as a flag indicating square can't be marked again
-            this.classList.add('x');                                    // add class "x", just changes background color
-            if( check_X() ) {                                           // check if 3 straight O's have occured
-                console.log("X winsies!");                              
-                alert("X Winsies!!  Press OK to start new game");       // if yes, alert game winner
-                turns = 0;                                              // reset turn count
-                resetSquareAll();                                       // reset HTML of all <li> squares
-            }
-        } 
+        else if ( turns % 2 === 1 ) {                               // if turn count is odd...
+            turns++;                                                
+            e.target.textContent = "x";                             // set inner text of element to "X"
+            e.target.classList.add('disable');                      // add class "disable" as a flag indicating square can't be marked again
+            e.target.classList.add('x');                            // add class "x", just changes background color
+            didXWin();
+        }
 
-
-        if ( turns === 9) {                                             // Always check if turn count is 9, if so declare tie game
+        if ( turns === 9) {                                         // if turn count is 9, declare tie game
             alert("Tie Game! Press OK to start a new game");
             turns = 0;
             resetSquareAll();
         }       
-    }) // end .addEventListener()
-}// end for loop
+    }
 
+    e.stopPropagation();                                            // stop event propogation
+}
 
-reset.addEventListener("click", resetSquareAll);                        // call resetSquareAll when reset button is clicked anytime
+reset.addEventListener("click", resetSquareAll);                    // call resetSquareAll when reset button is clicked anytime
